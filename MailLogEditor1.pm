9cf4438959511c5bdc25d1e809bc15a0ff283e46
Akiya Takahashi <atakahashi@ssri.com>
2019-01-10 16:53:42 +0900

    sum_create

--- MailLogEditor1.pm
+++ MailLogEditor1.pm
@@ -1,2 +1,30 @@
+#!/usr/bin/env perl
 package MailLogEditor1;
+use strict;
+
+sub foo {"BARRR"}
+sub hoehoe {"hoehoe"}
+
+sub sum {
+  my ($this, @num) = @_;
+  my $sum = shift @num;
+  foreach my $val (@num) {
+    $sum += $val;
+  }
+  $sum;
+}
+
+# unless (caller) {
+#   print foo(), "\n";
+# }
+
+# unless (caller) {
+#   print "ARGV is: ", @ARGV, "\n";
+# }
+
+unless (caller) {
+  my $method = $ARGV[0];
+  print MailLogEditor1->$method(@ARGV[1..$#ARGV]), "\n";
+}
+
 1;
