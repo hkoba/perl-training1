cb4e529424428b12f3a7937a44cd40eb2d41a95d
Akiya Takahashi <atakahashi@ssri.com>
2019-01-16 18:20:15 +0900

    動くようになった。。。

--- MailLogReader3.pm
+++ MailLogReader3.pm
@@ -4,8 +4,40 @@ use strict;
 
 use Data::Dumper;
 
-sub group_by_queueid{
+sub emit_sql_insert0 {
   my ($this, @files) = @_;
+  print "BEGIN;/n";
+  $this->do_group_by_queueid(
+    sub {
+      my ($queue) = @_;
+      my $VALUES = join(",", map {
+	defined $_ ? "'$_'" :"NULL"; #ここがよくわからない
+      } $queue->{queueid}, $queue->{'message-id'}, $queue->{uid}, $queue->{client});
+      print qq{insert into maillog(queue_id, message_id, uid, client) values($VALUES);\n};
+    },
+    @files
+  );
+  print "END;\n";
+  "";
+}
+
+
+# 構造確認用の関数という認識であってますよね？
+sub group_by_queueid {
+  my ($this, @files) = @_;
+  $this->do_group_by_queueid( # ここもわからなかった...do_group_by_queueidに@fileを入れている?
+    sub {
+      my ($queue) = @_;
+      #print Dumper($queue), "\n";
+      my $to_data = $queue->{to};
+      print Dumper($to_data->{to}), "\n";
+    },
+    @files
+  );
+}
+
+sub do_group_by_queueid{
+  my ($this, $sub, @files) = @_;
   local @ARGV = @files;
   local $_;
 
@@ -31,19 +63,26 @@ sub group_by_queueid{
       my $comment = $1; # $1はどこから・・・
       
       my @elems = split /,/, $text;
-      my $kv = +{map {split /=/, $_, 2} @elems}; #$_,2なんの関数なのかがわからない
-      $kv->{comment} = $comment;
+      my $kv = +{map {split /=/, $_, 2} @elems}; #$_,2何者なのかがわからない
+      $kv->{comment} = $comment; #'status' => 'bounced',の構造を作っている？＄１が何者なのかわからなかったのでここも理解できていなかった。
       
-      push @{$queue->{$key}}, $kv;
+      push @{$queue->{$key}}, $kv; # ifでつくった$key(to|from)に$kvを入れている？
       
     } elsif (($key, my $rest) = $text =~ m{^(uid|message-id|client)=(.*)}) { #=(.*)のぶぶんがどんな処理なのかわからない
       $queue->{$key} = $rest;
-    } elsif ($text eq 'removed') { #eqの処理内容がわからない
-      print Dumper($queue), "\n";# subを呼び出すというのがどういった処理になるのかがわからない
+    } elsif ($text eq 'removed') {
+      $sub->($queue); # subを呼び出すというのがどういった処理になるのかがわからない
+      # print Dumper($queue), "\n";
     } else {
       push @{$queue->{other}}, $text;
     }
   }
+}
+
+unless (caller) {
+  my $method = $ARGV[0];
+  print MailLogReader3->$method(@ARGV[1..$#ARGV]), "\n";
+  print "\n";
 }
 
 1;
