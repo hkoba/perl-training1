c5ab20fc5e1a6b421f76d9767fed0fbfc0626d79
Akiya Takahashi <atakahashi@ssri.com>
2019-01-17 16:23:16 +0900

    queue_id 以外は null の可能性がある

--- schema/maillog.sql
+++ schema/maillog.sql
@@ -1,14 +1,13 @@
 CREATE TABLE maillog(queue_id text, message_id text, uid text, client text);
 CREATE TABLE to_data 
 (queue_id text not null
-, status text not null
-, to_address text not null
-, delays text not null
-, comment text not null
-, delay text not null
-, dsn text not null
-, relay text not null
-, text_data text
-, primary key (queue_id)
+, status text null
+, to_address text null
+, delays text null
+, comment text null
+, delay text null
+, dsn text null
+, relay text null
+, text_data text null
 , foreign key (queue_id) references maillog  (queue_id)
 );
