c01b62dfab7b91cb94e0350da2dc42dff1c8768d
Akiya Takahashi <atakahashi@ssri.com>
2019-01-21 16:10:07 +0900

    ログの年度が変わったときのための処理。一日分より長くログが巻き戻ったら年を増やす

--- MailLogReader3.pm
+++ MailLogReader3.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 package MailLogReader3;
 use strict;
-use fields qw/year/;
+use fields qw/year _prev_epoch/;
 
 sub QItem () {'MailLogReader3::QItem'}
 package MailLogReader3::QItem {
@@ -111,9 +111,17 @@ sub group_by_queueid {
 our %MONTH = qw(Jan 1 Feb 2 Mar 3 Apr 4 May 5 Jun 6 Jul 7 Aug 8 Sep 9 Oct 10 Nov 11 Dec 12);
 
 sub epoch_from_localtime {
-  (my MY $this, my ($S, $M, $H, $day, $monthName, $year)) = @_;
-  timelocal($S, $M, $H, $day, $MONTH{$monthName} - 1
-	      , $year // $this->{year});
+  (my MY $self, my ($S, $M, $H, $day, $monthName, $year)) = @_;
+  my $yearRef = defined $year ? \$year : \$self->{year};
+  my $epoch = timelocal($S, $M, $H, $day, $MONTH{$monthName} - 1
+			, $$yearRef);
+  if ($self->{_prev_epoch} and ($self->{_prev_epoch} - $epoch) > (3600*24)) {
+    $$yearRef++;
+    $epoch = timelocal($S, $M, $H, $day, $MONTH{$monthName} - 1
+			 , $$yearRef);
+  }
+  $self->{_prev_epoch} = $epoch;
+  $epoch;
 }
 
 sub do_group_by_queueid{
