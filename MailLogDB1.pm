0c0422a9b78c80ec6b881e07a77081c7e38ef64d
Akiya Takahashi <atakahashi@ssri.com>
2019-01-25 14:24:03 +0900

    DB 接続メソッドを用意

--- MailLogDB1.pm
+++ MailLogDB1.pm
@@ -2,7 +2,23 @@
 package MailLogDB1;
 use strict;
 use MOP4Import::Base::CLI_JSON -as_base
-  ;
+  , [fields =>
+       qw/dbname
+	  _DBH
+	 /
+   ];
+
+use DBI;
+
+sub DB {
+  (my MY $self) = @_;
+
+  $self->{_DBH} //= do {
+    # 初回の DB() の呼び出し時に SQLite への接続を作る
+    DBI->connect("dbi:SQLite:dbname=$self->{dbname}", '', '',
+		 {PrintError => 0, RaiseError => 1, AutoCommit => 1});
+  };
+}
 
 MY->run(\@ARGV) unless caller;
 
