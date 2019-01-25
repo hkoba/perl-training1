79c1cbbfd528fdfe7e43630d802b4016893f4321
Akiya Takahashi <atakahashi@ssri.com>
2019-01-25 11:21:32 +0900

    year の help メッセージとデフォルト値の設定。
    * cget year で初期値が読めることを確認した。

--- MailLogReader4.pm
+++ MailLogReader4.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 package MailLogReader4;
 use strict;
+use utf8;
 
 # BEGIN {
 #   unshift @INC, "......./training1/";
@@ -8,10 +9,20 @@ use strict;
 use File::AddInc;
 
 use MOP4Import::Base::CLI_JSON -as_base
-  , [fields => qw/year
-		  foo
+  , [fields => ['year' => doc => "年度を指定"],
+	       qw/foo
 		  bar
 		  _prev_epoch/];
+
+use Time::Piece;
+
+#========================================
+
+sub after_new {
+  (my MY $self) = @_;
+  $self->SUPER::after_new;
+  $self->{year} //= localtime->year;
+}
 
 MY->run(\@ARGV) unless caller;
 
