8968dff3eb0b5721dba5990ba75f42c1ac0db32b
Akiya Takahashi <atakahashi@ssri.com>
2019-01-25 11:24:56 +0900

    epoch_from_localtime を持ってきてみた

--- MailLogReader4.pm
+++ MailLogReader4.pm
@@ -15,6 +15,8 @@ use MOP4Import::Base::CLI_JSON -as_base
 		  _prev_epoch/];
 
 use Time::Piece;
+use Time::Local;
+
 
 #========================================
 
@@ -23,6 +25,26 @@ sub after_new {
   $self->SUPER::after_new;
   $self->{year} //= localtime->year;
 }
+
+#========================================
+
+our %MONTH = qw(Jan 1 Feb 2 Mar 3 Apr 4 May 5 Jun 6 Jul 7 Aug 8 Sep 9 Oct 10 Nov 11 Dec 12);
+
+sub epoch_from_localtime {
+  (my MY $self, my ($S, $M, $H, $day, $monthName, $year)) = @_;
+  my $yearRef = defined $year ? \$year : \$self->{year};
+  my $epoch = timelocal($S, $M, $H, $day, $MONTH{$monthName} - 1
+			, $$yearRef);
+  if ($self->{_prev_epoch} and ($self->{_prev_epoch} - $epoch) > (3600*24)) {
+    $$yearRef++;
+    $epoch = timelocal($S, $M, $H, $day, $MONTH{$monthName} - 1
+			 , $$yearRef);
+  }
+  $self->{_prev_epoch} = $epoch;
+  $epoch;
+}
+
+#========================================
 
 MY->run(\@ARGV) unless caller;
 
