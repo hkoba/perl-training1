840a07e0242bd3cdfb27b5aefd1c9e0699f7e98e
Akiya Takahashi <atakahashi@ssri.com>
2019-01-25 11:10:56 +0900

    CLI_JSON の基本形＋ File::AddInc で同一ディレクトリの perl module を use 可能に

--- MailLogReader4.pm
+++ MailLogReader4.pm
@@ -1,3 +1,14 @@
 #!/usr/bin/env perl
 package MailLogReader4;
+use strict;
+
+# BEGIN {
+#   unshift @INC, "......./training1/";
+# }
+use File::AddInc;
+
+use MOP4Import::Base::CLI_JSON -as_base;
+
+MY->run(\@ARGV) unless caller;
+
 1;
